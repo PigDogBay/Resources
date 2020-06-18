3a42a70934f02d8732ee44d3a08336ea7d423cf5
mark <mpdbailey.technology@gmail.com>
2020-06-18 12:23:27 +0100

    added more commands to script, app icon inkscape document now fits content

--- csking/appicons.sh
+++ csking/appicons.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+rm -f ~/tmp/appicon-*.png
+rm -f ~/tmp/icons.zip
+
 inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-29.png --export-width=29 --export-height=29
 inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-58.png --export-width=58 --export-height=58
 inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-20.png --export-width=20 --export-height=20
@@ -12,3 +15,9 @@ inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filenam
 inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-167.png --export-width=167 --export-height=167
 inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-180.png --export-width=180 --export-height=180
 inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-1024.png --export-width=1024 --export-height=1024
+#Remove alpha
+find ~/tmp -name "appicon-*.png" -exec convert "{}" -alpha off "{}" \;
+#Show file png properties
+file ~/tmp/appicon-1024.png
+#zip up
+zip ~/tmp/icons.zip -j ~/tmp/appicon-*.png
