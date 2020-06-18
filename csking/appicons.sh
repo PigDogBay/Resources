918a0145f3fdc7a4a41b05d2c9aca9d60eb6fba9
mark <mpdbailey.technology@gmail.com>
2020-06-18 11:14:45 +0100

    script to create csk icons for iOS

---
+++ csking/appicons.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-29.png --export-width=29 --export-height=29
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-58.png --export-width=58 --export-height=58
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-20.png --export-width=20 --export-height=20
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-60.png --export-width=60 --export-height=60
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-76.png --export-width=76 --export-height=76
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-40.png --export-width=40 --export-height=40
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-80.png --export-width=80 --export-height=80
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-87.png --export-width=87 --export-height=87
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-120.png --export-width=120 --export-height=120
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-152.png --export-width=152 --export-height=152
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-167.png --export-width=167 --export-height=167
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-180.png --export-width=180 --export-height=180
+inkscape /home/mark/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/home/mark/tmp/appicon-1024.png --export-width=1024 --export-height=1024
