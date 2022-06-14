f3810681682cbc550deceaa84460b190962b3326
Mark Bailey <markpdbailey@gmail.com>
2022-06-14 13:50:11 +0100

    Updated CSK-iOS screen shots

--- csking/ios-screenshots/build.sh
+++ csking/ios-screenshots/build.sh
@@ -7,13 +7,13 @@
 #
 #File Structure:
 #
-#5.5 iPhone 8 Plus/ 
-#6.5 iPhone 11 Pro Max/ 
-#9.7 iPad/ 
+#iPhone8Plus-5.5/ 
+#iPhone11ProMax-6.5/ 
+#iPadPro4thGen-12.9/ 
 #build.sh
-#template 55.svg
-#template 65.svg
-#template 129.svg
+#template55.svg				1242x2208
+#template65.svg				1242x2688
+#template129.svg			2048x2732
 #
 #Note that the 12.9" app store screenshots will use the full size 9.7" images
 
@@ -29,12 +29,14 @@ rm -f appStore129/screenshot*.*
 
 echo 5.5 Inch Screenshots
 #Create svg files
-sed -e "s/TEXT1/Feature Packed/g; s/TEXT2/Easy To Use/g; s/FILENAME/ahome/g;" template55.svg > appStore55/screenshot1.svg
+sed -e "s/TEXT1/Solve Crosswords/g; s/TEXT2/Find Anagrams/g; s/FILENAME/ahome/g;" template55.svg > appStore55/screenshot1.svg
 sed -e "s/TEXT1/Find Words With/g; s/TEXT2/Miss?ng Lett?rs/g; s/FILENAME/bresults/g;" template55.svg > appStore55/screenshot2.svg
-sed -e "s/TEXT1/Powerful Filters/g; s/TEXT2/Refine Your Search/g; s/FILENAME/cfilter/g;" template55.svg > appStore55/screenshot3.svg
-sed -e "s/TEXT1/Filtered/g; s/TEXT2/Results/g; s/FILENAME/dfiltered-results/g;" template55.svg > appStore55/screenshot4.svg
-sed -e "s/TEXT1/Look Up Word/g; s/TEXT2/Definitions Online/g; s/FILENAME/fwiki/g;" template55.svg > appStore55/screenshot5.svg
-sed -e "s/TEXT1/Anagrams and/g; s/TEXT2/Sub-anagrams/g; s/FILENAME/ganagrams/g;" template55.svg > appStore55/screenshot6.svg
+sed -e "s/TEXT1/Thesaurus/g; s/TEXT2/To Solve Clues/g; s/FILENAME/cthesaurus/g;" template55.svg > appStore55/screenshot3.svg
+sed -e "s/TEXT1/Powerful Filters/g; s/TEXT2/Refine Your Search/g; s/FILENAME/dfilter/g;" template55.svg > appStore55/screenshot4.svg
+sed -e "s/TEXT1/Look Up Word/g; s/TEXT2/Definitions Online/g; s/FILENAME/ewiki/g;" template55.svg > appStore55/screenshot5.svg
+sed -e "s/TEXT1/Anagrams and/g; s/TEXT2/Sub-anagrams/g; s/FILENAME/fanagrams/g;" template55.svg > appStore55/screenshot6.svg
+sed -e "s/TEXT1/Built-in/g; s/TEXT2/Help and Examples/g; s/FILENAME/ghelp/g;" template55.svg > appStore55/screenshot7.svg
+sed -e "s/TEXT1/100,000 Phrases/g; s/TEXT2/To Search/g; s/FILENAME/hphrases/g;" template55.svg > appStore55/screenshot8.svg
 
 #Generate PNGs
 echo Screen 1
@@ -49,15 +51,21 @@ echo Screen 5
 /Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore55/screenshot5.png" appStore55/screenshot5.svg &>/dev/null
 echo Screen 6
 /Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore55/screenshot6.png" appStore55/screenshot6.svg &>/dev/null
+echo Screen 7
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore55/screenshot7.png" appStore55/screenshot7.svg &>/dev/null
+echo Screen 8
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore55/screenshot8.png" appStore55/screenshot8.svg &>/dev/null
 
 echo 6.5 Inch Screenshots
 #Create svg files
-sed -e "s/TEXT1/Feature Packed/g; s/TEXT2/Easy To Use/g; s/FILENAME/ahome/g;" template65.svg > appStore65/screenshot1.svg
+sed -e "s/TEXT1/Solve Crosswords/g; s/TEXT2/Find Anagrams/g; s/FILENAME/ahome/g;" template65.svg > appStore65/screenshot1.svg
 sed -e "s/TEXT1/Find Words With/g; s/TEXT2/Miss?ng Lett?rs/g; s/FILENAME/bresults/g;" template65.svg > appStore65/screenshot2.svg
-sed -e "s/TEXT1/Powerful Filters/g; s/TEXT2/Refine Your Search/g; s/FILENAME/cfilter/g;" template65.svg > appStore65/screenshot3.svg
-sed -e "s/TEXT1/Filtered/g; s/TEXT2/Results/g; s/FILENAME/dfiltered-results/g;" template65.svg > appStore65/screenshot4.svg
-sed -e "s/TEXT1/Look Up Word/g; s/TEXT2/Definitions Online/g; s/FILENAME/fwiki/g;" template65.svg > appStore65/screenshot5.svg
-sed -e "s/TEXT1/Anagrams and/g; s/TEXT2/Sub-anagrams/g; s/FILENAME/ganagrams/g;" template65.svg > appStore65/screenshot6.svg
+sed -e "s/TEXT1/Thesaurus/g; s/TEXT2/To Solve Clues/g; s/FILENAME/cthesaurus/g;" template65.svg > appStore65/screenshot3.svg
+sed -e "s/TEXT1/Powerful Filters/g; s/TEXT2/Refine Your Search/g; s/FILENAME/dfilter/g;" template65.svg > appStore65/screenshot4.svg
+sed -e "s/TEXT1/Look Up Word/g; s/TEXT2/Definitions Online/g; s/FILENAME/ewiki/g;" template65.svg > appStore65/screenshot5.svg
+sed -e "s/TEXT1/Anagrams and/g; s/TEXT2/Sub-anagrams/g; s/FILENAME/fanagrams/g;" template65.svg > appStore65/screenshot6.svg
+sed -e "s/TEXT1/Built-in/g; s/TEXT2/Help and Examples/g; s/FILENAME/ghelp/g;" template65.svg > appStore65/screenshot7.svg
+sed -e "s/TEXT1/100,000 Phrases/g; s/TEXT2/To Search/g; s/FILENAME/hphrases/g;" template65.svg > appStore65/screenshot8.svg
 
 #Generate PNGs
 echo Screen 1
@@ -72,17 +80,22 @@ echo Screen 5
 /Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore65/screenshot5.png" appStore65/screenshot5.svg &>/dev/null
 echo Screen 6
 /Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore65/screenshot6.png" appStore65/screenshot6.svg &>/dev/null
+echo Screen 7
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore65/screenshot7.png" appStore65/screenshot7.svg &>/dev/null
+echo Screen 8
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore65/screenshot8.png" appStore65/screenshot8.svg &>/dev/null
 
 
 echo 12.9 Inch Screenshots
 #Create svg files
-sed -e "s/TEXT1/Feature Packed/g; s/TEXT2/Easy To Use/g; s/FILENAME/ahome/g;" template129.svg > appStore129/screenshot1.svg
+sed -e "s/TEXT1/Solve Crosswords/g; s/TEXT2/Find Anagrams/g; s/FILENAME/ahome/g;" template129.svg > appStore129/screenshot1.svg
 sed -e "s/TEXT1/Find Words With/g; s/TEXT2/Miss?ng Lett?rs/g; s/FILENAME/bresults/g;" template129.svg > appStore129/screenshot2.svg
-sed -e "s/TEXT1/Powerful Filters/g; s/TEXT2/Refine Your Search/g; s/FILENAME/cfilter/g;" template129.svg > appStore129/screenshot3.svg
-#sed -e "s/TEXT1/Filtered/g; s/TEXT2/Results/g; s/FILENAME/dfiltered-results/g;" template129.svg > appStore129/screenshot4.svg
-sed -e "s/TEXT1/Filtered/g; s/TEXT2/Results/g; s/FILENAME/elookup/g;" template129.svg > appStore129/screenshot4.svg
-sed -e "s/TEXT1/Look Up Word/g; s/TEXT2/Definitions Online/g; s/FILENAME/fwiki/g;" template129.svg > appStore129/screenshot5.svg
-sed -e "s/TEXT1/Anagrams and/g; s/TEXT2/Sub-anagrams/g; s/FILENAME/ganagrams/g;" template129.svg > appStore129/screenshot6.svg
+sed -e "s/TEXT1/Thesaurus/g; s/TEXT2/To Solve Clues/g; s/FILENAME/cthesaurus/g;" template129.svg > appStore129/screenshot3.svg
+sed -e "s/TEXT1/Powerful Filters/g; s/TEXT2/Refine Your Search/g; s/FILENAME/dfilter/g;" template129.svg > appStore129/screenshot4.svg
+sed -e "s/TEXT1/Look Up Word/g; s/TEXT2/Definitions Online/g; s/FILENAME/ewiki/g;" template129.svg > appStore129/screenshot5.svg
+sed -e "s/TEXT1/Anagrams and/g; s/TEXT2/Sub-anagrams/g; s/FILENAME/fanagrams/g;" template129.svg > appStore129/screenshot6.svg
+sed -e "s/TEXT1/Built-in/g; s/TEXT2/Help and Examples/g; s/FILENAME/ghelp/g;" template129.svg > appStore129/screenshot7.svg
+sed -e "s/TEXT1/100,000 Phrases/g; s/TEXT2/To Search/g; s/FILENAME/hphrases/g;" template129.svg > appStore129/screenshot8.svg
 
 #Generate PNGs
 echo Screen 1
@@ -97,4 +110,8 @@ echo Screen 5
 /Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore129/screenshot5.png" appStore129/screenshot5.svg &>/dev/null
 echo Screen 6
 /Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore129/screenshot6.png" appStore129/screenshot6.svg &>/dev/null
+echo Screen 7
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore129/screenshot7.png" appStore129/screenshot7.svg &>/dev/null
+echo Screen 8
+/Applications/Inkscape.app/Contents/MacOS/inkscape --export-area-page --export-png="appStore129/screenshot8.png" appStore129/screenshot8.svg &>/dev/null
 
