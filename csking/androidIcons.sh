2f7d22cd68cc373270eaa88e5e853ee7820d730e
Mark Bailey <markpdbailey@gmail.com>
2022-06-16 11:46:23 +0100

    script to generate android store icon

---
+++ csking/androidIcons.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/Applications/Inkscape.app/Contents/MacOS/inkscape /Users/markbailey/work/MPDBTech/Resources/csking/app_icon.svg --export-filename=/Users/markbailey/tmp/appicon-512.png --export-width=512 --export-height=512
+
